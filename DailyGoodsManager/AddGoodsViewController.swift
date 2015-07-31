b2b65e167eaf4975ae986f87996e64cb75f232ab
Ryo Morimoto <morimoto@M-3.local>
2015-07-31 22:19:07 +0900

    add GoodsListTableViewController add AddGoodsListViewController

---
+++ DailyGoodsManager/AddGoodsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AddGoodsViewController.swift
+//  DailyGoodsManager
+//
+//  Created by Ryo Morimoto on 2015/07/31.
+//  Copyright (c) 2015年 Ryo Morimoto. All rights reserved.
+//
+
+import UIKit
+
+class AddGoodsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
